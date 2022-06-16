094e09385252a4c9d0c5c81cb7026494533f672e
sgao <sgao@beckman.com>
2022-06-16 08:30:31 +0800

    update

---
+++ lambda_m_threads_test.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+echo '2 threads 5m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=2 --bs=1m --size=5m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '2 threads 10m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=2 --bs=1m --size=10m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '2 threads 25m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=2 --bs=1m --size=25m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '2 threads 50m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=2 --bs=1m --size=50m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '2 threads 100m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=2 --bs=1m --size=100m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '2 threads 200m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=2 --bs=1m --size=200m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+
+echo '5 threads 5m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=5 --bs=1m --size=5m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '5 threads 10m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=5 --bs=1m --size=10m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '5 threads 25m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=5 --bs=1m --size=25m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '5 threads 50m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=5 --bs=1m --size=50m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '5 threads 100m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=5 --bs=1m --size=100m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '5 threads 200m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=5 --bs=1m --size=200m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+
+echo '10 threads 5m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=10 --bs=1m --size=5m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '10 threads 10m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=10 --bs=1m --size=10m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '10 threads 25m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=10 --bs=1m --size=25m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '10 threads 50m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=10 --bs=1m --size=50m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '10 threads 100m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=10 --bs=1m --size=100m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '10 threads 200m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=10 --bs=1m --size=200m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+
+echo '15 threads 5m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=15 --bs=1m --size=5m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '15 threads 10m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=15 --bs=1m --size=10m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '15 threads 25m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=15 --bs=1m --size=25m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '15 threads 50m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=15 --bs=1m --size=50m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '15 threads 100m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=15--bs=1m --size=100m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '15 threads 200m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=15 --bs=1m --size=200m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+
+echo '20 threads 5m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=20 --bs=1m --size=5m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '20 threads 10m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=20 --bs=1m --size=10m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '20 threads 25m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=20 --bs=1m --size=25m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '20 threads 50m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=20 --bs=1m --size=50m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '20 threads 100m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=20 --bs=1m --size=100m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
+echo '20 threads 200m file - search flag'
+fio --ioengine=libaio --iodepth=8 --readwrite=read --numjobs=20 --bs=1m --size=200m --name=job0 --filename=/mnt/channelstripe/testfile_read_job0 --thread
--- src/main/java/org/cytobank/test/util/PropertiesUtils.java
+++ src/main/java/org/cytobank/test/util/PropertiesUtils.java
@@ -3,7 +3,6 @@ package org.cytobank.test.util;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -23,8 +22,8 @@ public class PropertiesUtils {
    */
   public static <T> T loadProperties(Class<T> clazz, String yml) {
     try {
-      InputStream inputStream = new FileInputStream("./" + yml);
-//      InputStream inputStream = clazz.getResourceAsStream("/" + yml);
+//      InputStream inputStream = new FileInputStream("./" + yml);
+      InputStream inputStream = clazz.getResourceAsStream("/" + yml);
       return mapper.readValue(inputStream, clazz);
     } catch (IOException e) {
       e.printStackTrace();
